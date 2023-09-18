1ee02efc7f8afa1a8bb71192c96b169bf0c25095
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-09-17 22:55:06 -0700

    add more custom options for spiral

--- turtle/animation.py
+++ turtle/animation.py
@@ -1,60 +1,64 @@
 import turtle
 import colorsys
 
+
 class Spiral(turtle.Turtle):
-    def __init__(self, location = (0, 0), scale = 1, spin_speed = 1, start_hue = 0, color_change = 0.004) -> None:
+    def __init__(self, location: tuple[int, int] = (0, 0), scale = 1.0, color_change = 1.0, num_of_segments = 4, segment_gap = 10, number_of_arms = 1) -> None:
         super().__init__(visible=False)
-        self.pensize(4)
-       
+
         self.location = location
         self.scale = scale
-        self.spin_speed = spin_speed
-       
-        self.start_hue = start_hue
-        self.color_change = color_change
-       
+        self.color_change = 0.004 * color_change
+        self.num_of_segments = num_of_segments
+        self.segment_gap = segment_gap
+        self.number_of_arms = number_of_arms
+
+        self.hideturtle()
+        self.pensize(4)
         self.reset()
-       
+
     def tp(self, location):
         self.penup()
         self.goto(location)
         self.pendown()
-       
+
     def reset(self) -> None:
         super().reset()
-        self.hue = self.start_hue
+        self.hue = 0
         self.tp(self.location)
-       
+
     def draw_shape(self) -> None:
         self.forward(50 * self.scale)
-        self.right(20 * self.scale)
+        self.right(20)
         self.forward(40 * self.scale)
-        self.right(9 * self.scale)
-       
+        self.right(9)
+
     def next(self) -> None:
-        for i in range(4):            
+        start_heading = self.heading()
+        
+        for i in range(self.num_of_segments):
             color = colorsys.hsv_to_rgb(self.hue, 1, 1)
             self.fillcolor(color)
-            self.hue += self.color_change
-           
+
             self.begin_fill()
             self.draw_shape()
             self.end_fill()
-   
-        self.right(self.spin_speed)
+
+            self.hue += self.color_change
+
+        self.setheading(start_heading + self.segment_gap + (360 / self.number_of_arms))
         self.tp(self.location)
+
 
 def main():
     turtle.bgcolor("black")
-    turtle.tracer(100)
-   
-    s = Spiral()
-   
-    for i in range(400):
-        s.next()
-       
-   
+    turtle.tracer(20)
 
+    spiral1 = Spiral(location=(0, 0), scale=1, color_change=0.5, num_of_segments=4, segment_gap=5, number_of_arms=3)
+    spiral2 = Spiral(location=(0, 0), scale=1, color_change=1, num_of_segments=4, segment_gap=10, number_of_arms=1)
+
+    while True:
+        spiral1.next()
 
 if __name__ == "__main__":
     main()
