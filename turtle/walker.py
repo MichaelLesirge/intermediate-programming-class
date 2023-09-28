35ed9595368de53650883d5dca53a996859c1ede
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-09-27 23:53:45 -0700

    walker graphing

--- turtle/walker.py
+++ turtle/walker.py
@@ -1,6 +1,10 @@
-import turtle
 import random
-        
+import turtle
+
+# do pip install matplotlib
+import matplotlib.pyplot as plt
+import numpy as np        
+
 class Walker():
     def __init__(self, shape_sides: int, distance) -> None:
         
@@ -10,19 +14,33 @@ class Walker():
         self.degrees = 360 / shape_sides  
         self.distance = distance
         
+        self.start_pos = self.tur.pos()
+        
         self.sides = shape_sides
                             
     def step(self):
         if random.randint(0, 1): self.tur.left(self.degrees)
         else: self.tur.forward(self.distance)
+    
+    def get_distance_from_start(self) -> float:
+        current_pos = self.tur.pos()
+        return ((self.start_pos[0] - current_pos[0]) ** 2 + (self.start_pos[1] - current_pos[1]) ** 2) ** 0.5
+        
+    def shape_step(self):
+        for i in range(self.sides):
+            self.tur.forward(self.distance)
+            self.tur.left(self.degrees)
+        
+        self.tur.left(random.randint(0, self.sides) * self.degrees)
+        self.tur.forward(self.distance)
          
 def main():
     
-    num_of_walkers = 10
+    num_of_walkers = 100
     # num_of_walkers = int(input("number of walker: "))
     shape_sides = 3
     # shape_sides = int(input("number of sides of shape: "))
-    num_of_steps = 1000
+    num_of_steps = 500
     # num_of_steps = int(input("number of steps: "))
     
     distance = 10
@@ -36,7 +54,15 @@ def main():
         for walker in walkers:
             walker.step()
         turtle.update()
+    
+    number_of_bins = 10
+    distances = [walker.get_distance_from_start() for walker in walkers]
+            
+    plt.hist(distances, bins=number_of_bins)
+    # plt.plot(range(len(distances)), sorted(distances))
 
+    plt.show()
+    
     turtle.mainloop()
     
 
