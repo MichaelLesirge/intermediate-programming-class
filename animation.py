8d271b0aa2ab7a15b00e041b6fa992067be49043
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-10-06 23:40:40 -0700

    redo some animation stuff

--- animation.py
+++ animation.py
@@ -1,3 +1,4 @@
+from typing import Any
 import pygame
 import random
 import math
@@ -7,8 +8,8 @@ pygame.init()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
-BOUNCE_RETENTION = 1
-GRAVITY = 1
+BOUNCE_RETENTION = 0.6
+GRAVITY = pygame.Vector2(0, 0.5)
 
 FPS = 60
 WHITE = (255, 255, 255)
@@ -17,7 +18,7 @@ BLACK = (0, 0, 0)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Falling Shapes")
 
-class Drop(pygame.sprite.Sprite):
+class Ball(pygame.sprite.Sprite):
     @classmethod
     def random_shape(cls):
         # location = (random.randint(0, SCREEN_WIDTH), 0)
@@ -39,63 +40,85 @@ class Drop(pygame.sprite.Sprite):
         
         area = math.pi * self.radius ** 2
         density = 0.0001
+        
         self.mass = area * density
-        
-        self.start_time_to_live = 50
-        
-        self.time_to_live = self.start_time_to_live
         
         self.velocity = pygame.Vector2(velocity) 
         
-    def set_random_color(self):
+    @property
+    def position(self) -> pygame.Vector2:
+        return pygame.Vector2(self.rect.center)
+    
+    @position.setter
+    def position(self, value) -> None:
+        self.rect.center = value
+        
+    def set_random_color(self) -> None:
         pygame.draw.circle(self.image, (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), (self.radius, self.radius), self.radius)
 
-    def update(self, floors: pygame.sprite.Group, other_balls: pygame.sprite.Group):
-        floors: list[Floor] = list(floors)
+    def update(self, floors_group: pygame.sprite.Group, other_balls_group: pygame.sprite.Group) -> None:
+        floors: list[Floor] = list(floors_group)
+        other_balls: list[Ball] = list(other_balls_group)
         
-        # https://www.youtube.com/watch?v=Ge1DbXkyMKo&ab_channel=griffpatch
-        self.velocity.y += GRAVITY
-                
-        self.rect.center += self.velocity
+        self.velocity += GRAVITY
         
-        bounce_back = 0
-        
-        for floor in floors:
-            if self.rect.colliderect(floor.rect):
-                bounce_back = self.velocity.y
-                
-                if floor.speed.x > 0: print(self.velocity, floor.speed)
-                
-                self.velocity.y -= floor.speed.y
-                self.velocity.y *= -0.6
-            
-        self.rect.centery -= bounce_back
+        self.position += self.velocity
             
         
     
 class Floor(pygame.sprite.Sprite):
-    def __init__(self, size: pygame.Vector2, location: pygame.Vector2 = (0, 0), follow_provider = None) -> None:
+    def __init__(self, size: pygame.Vector2, location: pygame.Vector2 = (0, 0)) -> None:
         super().__init__()
         
-        self.rotation = 0
+        self.angle = 0
+        self.mass = float("inf")
         
-        self.location_func = follow_provider
-
         self.speed = pygame.Vector2(0, 0)
         
-        self.image = pygame.Surface(size)
-        pygame.rect.Rect(location, pygame.Vector2(location) + size)
+        self.image_start = pygame.Surface(size)
+        self.rect = pygame.rect.Rect(location, pygame.Vector2(location) + size)
+        
+        self.image_start.fill(BLACK)
+        self.image = self.image_start
+    
+class Paddle(Floor):
+    def __init__(self, size: pygame.Vector2, location: pygame.Vector2 = (0, 0)) -> None:
+        super().__init__(size, location)
+        
+        change_size = 5
+        color = (0, 75, 214)
+        
+        self.image.fill(color)
+        
+        self.spin_speed = change_size
+        self.grow_size = change_size
+                
+        self.image.set_colorkey((30, 30, 30))
+        
+        self.should_move = False
+         
+    def update(self) -> None:
+        
+        keys = pygame.key.get_pressed()
+                
+        if keys[pygame.K_LEFT]:
+            self.angle = (self.angle + self.spin_speed) % 360
+        if keys[pygame.K_RIGHT]:
+            self.angle = (self.angle - self.spin_speed) % 360
+        # if keys[pygame.K_UP]:
+        #     self.width = min(self.width + self.grow_size, 100)
+        # if keys[pygame.K_DOWN]:
+        #     self.width - max(self.grow_size - self.grow_size, 0)
+        
+        self.image = pygame.transform.rotate(self.image_start, self.angle)
         self.rect = self.image.get_rect()
         
-        if not follow_provider: self.rect.topleft = location
-        self.image.fill(BLACK)
-    
-    def update(self) -> None:
-        if self.location_func:
-            new_center = pygame.Vector2(self.location_func())
-            self.speed = pygame.Vector2(abs(new_center.x - self.rect.centerx), abs(new_center.y - self.rect.centery))
-            self.rect.center = new_center
-        # self.rect.clamp_ip((0, 0), (SCREEN_WIDTH, SCREEN_HEIGHT))
+        if self.should_move:
+            self.rect.center = pygame.mouse.get_pos()
+        else:
+            self.rect.y = SCREEN_HEIGHT
+            self.should_move = pygame.mouse.get_pos() != (0, 0)
+                
 
 def main():
     shapes = pygame.sprite.Group()
@@ -108,18 +131,15 @@ def main():
     floor = Floor((SCREEN_WIDTH, SCREEN_HEIGHT * 0.1), (0, SCREEN_HEIGHT * 0.9))
     floors.add(floor)
     
-    paddle = Floor((100, 10), follow_provider=pygame.mouse.get_pos)
+    paddle = Paddle((100, 10))
     floors.add(paddle)
 
-    # new_shape = Drop.random_shape()
-    # shapes.add(new_shape)
-        
     while running:
         if pygame.event.get(pygame.QUIT):
             running = False
    
         if random.randint(0, 75) == 0:
-            new_shape = Drop.random_shape()
+            new_shape = Ball.random_shape()
             shapes.add(new_shape)
         
         shapes.update(floors, shapes)
