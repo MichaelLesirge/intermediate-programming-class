1095a1a76eed07e23ffd17d0fa7ad973f364f70d
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-10-07 21:37:42 -0700

    sorting plan

---
+++ sorting.py
@@ -0,0 +1,11 @@
+"""
+TODO
+
+Bubble, Insertion, Selection, Merge, and Quick Sort
+Option of shuffled mode and random number mode
+Beeps based on element size
+Display name of sorting, (comparisons, swaps) and (sets, gets)
+highlight relevant indexes
+
+graph time of sorting for different array sizes at end
+"""
