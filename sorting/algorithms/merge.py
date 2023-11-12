d6ace38d368639cb0e97abdc94b6389f36647bea
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-11-11 16:56:14 -0800

    clean up merge sort

--- sorting/algorithms/merge.py
+++ sorting/algorithms/merge.py
@@ -14,40 +14,35 @@ Cons:
 - uses extra memory for another array
 """
 
+
 def merge(array: list, left: int, mid: int, right: int) -> None:
     start1, end1 = left, mid
-    start2, end2 = mid + 1, right
-    
-    sorted_section = []
-        
-    while start1 <= end1 and start2 <= end2:
+    start2, end2 = mid, right
+
+    merged_area = []
+    while start1 < end1 and start2 < end2:
         if array[start1] < array[start2]:
-            sorted_section.append(array[start1])
+            merged_area.append(array[start1])
             start1 += 1
-        else:   
-            sorted_section.append(array[start2])
+        else:
+            merged_area.append(array[start2])
             start2 += 1
 
-    while start1 <= end1:
-        sorted_section.append(array[start1])
-        start1 += 1
+    if start1 < end1: merged_area.extend(array[start1:end1])
+    if start2 < end2: merged_area.extend(array[start2:end2])
 
-    while start2 <= end2:
-        sorted_section.append(array[start2])
-        start2 += 1
-    
-    array[left : right + 1] = sorted_section
-        
+    array[left:right] = merged_area
+
 
 def merge_sort(array: list, left: int = None, right: int = None) -> None:
     if left is None: left = 0
-    if right is None: right = len(array) - 1
-    
-    if left >= right: return
-    
-    mid = left + (right - left) // 2
-        
-    merge_sort(array, left, mid)
-    merge_sort(array, mid + 1, right)
+    if right is None: right = len(array)
 
-    merge(array, left, mid, right) 
+    if left >= right - 1: return
+
+    mid = left + (right - left) // 2
+
+    merge_sort(array, left, mid)
+    merge_sort(array, mid, right)
+
+    merge(array, left, mid, right)
