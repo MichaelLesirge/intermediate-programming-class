1173ad17d37cbd621be1343e7c6d884e4eccf4ef
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-11-11 23:41:17 -0800

    clean up quick sort

--- sorting/algorithms/quick.py
+++ sorting/algorithms/quick.py
@@ -14,33 +14,31 @@ Cons:
 
 """
 
-def swap(array: list, a: int, b: int) -> None:
-    array[a], array[b] = array[b], array[a]
+def swap(array: list, i: int, j: int) -> None:
+    array[i], array[j] = array[j], array[i]
 
-def partition(array: list, left: int, right: int) -> int:
-    pivot = array[right]
- 
-    i = left - 1
- 
-    for j in range(left, right):
-        if array[j] <= pivot:
- 
-            i += 1
-            
-            swap(array, i, j)
- 
-    swap(array, i + 1, right)
- 
-    return i + 1
- 
- 
-def quick_sort(array: list, left: int = None, right: int = None):
-    if left is None: left = 0
-    if right is None: right = len(array) - 1
+def partition(array: list, left: int, right: int) -> None:
+    # pivot index first, center, last, random, or median-of-three
+    pivot = right-1
+    pivot_value = array[pivot]
     
-    if left >= right: return
- 
-    partition_index = partition(array, left, right)
- 
-    quick_sort(array, left, partition_index - 1)
-    quick_sort(array, partition_index + 1, right)
+    pivot_center = left
+    
+    for i in range(left, right):
+        if array[i] < pivot_value:
+            swap(array, i, pivot_center)
+            pivot_center += 1
+            
+    swap(array, pivot, pivot_center)
+        
+    return pivot_center
+
+def quick_sort(array: list, left: int = None, right: int = None) -> None:
+    if left is None: left = 0
+    if right is None: right = len(array)
+    
+    if left >= right - 1: return
+    
+    pivot = partition(array, left, right)
+    quick_sort(array, left, pivot)
+    quick_sort(array, pivot+1, right)
